dfad7c74b63fe2417bce2cd90293bfabaa764462
Amit Kumar Sahu <68862579+Amit-Kumar-Sahu@users.noreply.github.com>
2021-06-01 12:27:19 +0530

    Update Birthday_Tune.ino

--- Birthday_Tune.ino
+++ Birthday_Tune.ino
@@ -3,6 +3,10 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
+#define PAUSE 0
+// end of custom
+
+
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // setup hw
